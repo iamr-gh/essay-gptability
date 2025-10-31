75b1538ee169e290758215f0d7d63cb303294207
Ibrahim Musaddequr Rahman <iamr@umich.edu>
2025-10-31 01:33:18 -0400

    made a 100 fake essays locally

--- main.py
+++ main.py
@@ -48,12 +48,12 @@ def write_essay(
 def main():
     df = pd.read_csv("ASAP2_train_sourcetexts.csv")
 
-    to_consider = 10
+    to_consider = 100
     # there is multiple human answers per prompt
 
+    ai_answers = []
     # there's multiple source texts
-    for ii in range(to_consider):
-        i = ii * 5000
+    for i in range(to_consider):
         # context window might not be long enough
         ai_answer = write_essay(
             df["prompt_name"][i],
@@ -64,6 +64,8 @@ def main():
             df["source_text_4"][i],
         )
 
+        ai_answers.append(ai_answer)
+
         yellow_begin = "\033[93m"
         green_begin = "\033[92m"
         blue_begin = "\033[94m"
@@ -73,8 +75,24 @@ def main():
             f"\n\n Assignment:{df['assignment'][i]}\n\n {green_begin}AI answer:\n{ai_answer} {end}, \n\n {blue_begin} Human answer:\n{df['full_text'][i]} {end}"
         )
 
+    out_df = pd.DataFrame(
+        {
+            "ai_output": ai_answers,
+            "human_output": df["full_text"][:to_consider],
+            "prompt_name": df["prompt_name"][:to_consider],
+            "assignment": df["assignment"][:to_consider],
+            "source_text_1": df["source_text_1"][:to_consider],
+            "source_text_2": df["source_text_2"][:to_consider],
+            "source_text_3": df["source_text_3"][:to_consider],
+            "source_text_4": df["source_text_4"][:to_consider],
+        }
+    )
+
+    out_df.to_csv("generated.csv", index=False)
+
     # output data I need at this point
     # ai output, human output, prompt info(name, assignment, source texts)
+    # and then for classification I'll split into labels
 
     breakpoint()
 
